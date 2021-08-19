3e9e3f462e2c694859e6ab18048b729940d4632a
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-08-18 23:35:22 -0300

    criacao da lista

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,16 +1,31 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pokedexapp/const/const_app.dart';
+import 'package:pokedexapp/models/pokeapi.dart';
 import 'package:pokedexapp/pages/home_page/widgets/app_bar_home.dart';
+import 'package:pokedexapp/stores/pokeapi_store.dart';
 
-class HomePage extends StatelessWidget {
-  
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PokeApiStore pokeApiStore;
+
+  @override
+  void initState() {
+    super.initState();
+    pokeApiStore = PokeApiStore();
+    pokeApiStore.fetchPokemonList();
+  }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
@@ -29,14 +44,34 @@ class HomePage extends StatelessWidget {
           AppbarHomeWidget(),
           Container(
             margin: EdgeInsets.only(top: screenWidth * 0.55),
-            height: 100,
             decoration: BoxDecoration(
-              color: Color.fromRGBO(238, 27, 36, 0.7),
+              color: Color.fromRGBO(238, 27, 36, 1),
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(45),
                 topRight: Radius.circular(45),
               ),
             ),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02, vertical: screenHeight * 0.02),
+              child: Observer(
+              name: 'ListaHomePage',
+              builder: (BuildContext context) {
+                PokeAPI _pokeApi = pokeApiStore.pokeAPI;
+                return (_pokeApi != null)
+                    ? ListView.builder(
+                        itemCount: _pokeApi.pokemon.length,
+                        itemBuilder: (contex, index) {
+                          return ListTile(
+                            title: Text('${_pokeApi.pokemon[index].name}'),
+                          );
+                        })
+                    : Center(
+                        child: CircularProgressIndicator(
+                          color: Colors.white,
+                        ),
+                      );
+              },
+            )),
           ),
         ],
       ),
